8679e9c2de534f31042bcb2d3309068bde301cc0
Facundo Padula <facundo.padula@cedarcode.com>
2019-10-02 13:09:55 -0300

    Rename script

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -148,7 +148,6 @@ class Auth::SessionsController < Devise::SessionsController
         session.delete(:webauthn_user_id)
         remember_me(user)
         sign_in(user)
-        byebug
         render json: { redirect_path: root_path }, status: :ok
       else
         flash.now[:alert] = "Invalid security key!"
