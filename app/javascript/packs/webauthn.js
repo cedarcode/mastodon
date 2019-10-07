2e6ab51232a4e9c1b4c0cd4c4c07f7c9b93b70af
Facundo Padula <facundo.padula@cedarcode.com>
2019-10-07 10:37:00 -0300

    Merge webauth js code into one file

--- app/javascript/packs/webauthn.js
+++ app/javascript/packs/webauthn.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import * as WebAuthnJSON from "@github/webauthn-json";
+import ready from '../mastodon/ready';
 
 function getCSRFToken() {
   var CSRFSelector = document.querySelector('meta[name="csrf-token"]')
@@ -25,4 +27,47 @@ function callback(url, body) {
   });
 }
 
-export { callback }
+ready(() => {
+  const registration_form = document.getElementById('new_webauthn_credential');
+  if (registration_form) {
+    registration_form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      var nickname = event.target.querySelector("input[name='new_webauthn_credential[nickname]']");
+      if (!nickname.value) {
+        nickname.focus()
+        return false
+      }
+
+      axios.get('/settings/two_factor_authentication/security_keys/options')
+        .then((response) => {
+          const credentialOptions = response.data;
+
+          WebAuthnJSON.create({ "publicKey": credentialOptions }).then((credential) => {
+            var params = { "credential": credential, "nickname": nickname.value }
+            callback('/settings/two_factor_authentication/security_keys', params)
+          }).catch((error) => {
+            console.log(error);
+          });
+        })
+    });
+  }
+
+  const authentication_form = document.getElementById('edit_user');
+  if (authentication_form) {
+    authentication_form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      axios.get('sessions/security_key_options')
+        .then((response) => {
+          const credentialOptions = response.data;
+
+          WebAuthnJSON.get({ "publicKey": credentialOptions }).then((credential) => {
+            callback("sign_in", { "credential": credential });
+          }).catch((error) => {
+            console.log(error);
+          });
+        })
+    });
+  }
+});
